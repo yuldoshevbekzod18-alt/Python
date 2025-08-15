53ac9e88581334b98e060dcd100bc6e7a6e26d72
yuldoshevbekzod18-alt <yuldoshevbekzod18@gmail.com>
2025-08-15 10:20:34 +0500

    Update main.py

--- lesson-1/homework/main.py
+++ lesson-1/homework/main.py
@@ -1,3 +1,31 @@
-# 1.
-a = 5
-print(4 * a)
+#Given a side of square. Find its perimeter and area.
+a=10
+peremetr=4*a
+area=a**2
+print(peremetr)
+print(area)
+
+
+#Given diameter of circle. Find its length.
+d=int(input('d sonini kiriting:'))
+p=3.14
+L=d*p
+print(L)
+
+#Given two numbers a and b. Find their mean.
+a=float(input("a sonini kiriting: "))
+b=float(input('b sonini kiriting:'))
+mean=a+b/2
+print(mean)
+
+#Given two numbers a and b. Find their sum, product and square of each number.
+a=int(input('a sonini kiriting:'))
+b=int(input('b sonini kiriting:'))
+sum=a+b
+product=a*b
+a_sqr=a**2
+b_sqr=b**2
+print(sum)
+print(product)
+print(a_sqr)
+print(b_sqr)
